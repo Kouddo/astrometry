fcaf70057c96b4739fdf191aaffb926ad5c458e2
abhi <46144798+Kouddo@users.noreply.github.com>
2022-10-25 16:19:13 -0700

    updated dark processing

--- preprocessing/darks.py
+++ preprocessing/darks.py
@@ -3,10 +3,18 @@ from astropy.io import fits
 import numpy
 
 from os import listdir
-from os.path import isfile,
 
 
 def dark_gen(filepath):
-    files = [fits.open(i) for i in listdir(filepath) if i.endswith.lower(('.fits'))]
-    for i in files 
+    files = [fits.open(filepath + '/' + i)[0] for i in listdir(filepath) if i.endswith('.fits')]
+    for i in files:
+        assert files[0].header['GAIN'] == i.header['GAIN']
+        assert files[0].header['EXPOSURE'] == i.header['EXPOSURE']
+        assert files[0].header['INSTRUME'] == i.header['INSTRUME']
+    
+    master = sum([i.data for i in files])/len(files)
+    return master
+
+    
+
 
